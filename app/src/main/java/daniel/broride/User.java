6d55af24358c985adce82e87760bda6f80df7371
danielbibit <danielbibit@gmail.com>
2016-12-16 15:56:42 -0200

    Added gets an setters for User and Vehicle class Updated Vehicle class

--- app/src/main/java/daniel/broride/User.java
+++ app/src/main/java/daniel/broride/User.java
@@ -7,5 +7,36 @@ public class User {
     private double debit;
     private boolean driver = false;
 
+    //Gets and setters
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getDebit() {
+        return debit;
+    }
+
+    public void setDebit(double debit) {
+        this.debit = debit;
+    }
+
+    public boolean isDriver() {
+        return driver;
+    }
+
+    public void setDriver(boolean driver) {
+        this.driver = driver;
+    }
 }
