745f1dad2caea082032d4e30e49f9c45658dbb74
danielbibit <danielbibit@gmail.com>
2016-12-16 16:08:52 -0200

    Added table structures

--- app/src/main/java/daniel/broride/DbHelper.java
+++ app/src/main/java/daniel/broride/DbHelper.java
@@ -8,6 +8,28 @@ public class DbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "main.db";
 
+    public static final String TABLE_USER = "USER_TABLE";
+    public static final String USER_ID = "ID";
+    public static final String USER_NAME = "NAME";
+    public static final String USER_DRIVER = "DRIVER";
+    public static final String USER_AGE = "AGE";
+    public static final String USER_DEBIT = "DEBIT";
+
+    public static final String TABLE_VEHICLE = "USER_VEHICLE";
+    public static final String VEHICLE_ID = "ID";
+    public static final String VEHICLE_MODEL = "MODEL";
+    public static final String VEHICLE_NAME = "NAME";
+    public static final String VEHICLE_CAPACITY = "CAPACITY";
+    public static final String VEHICLE_CONSUMPTION = "CONSUMPTION";
+
+    /*
+    public static final String TABLE_RIDE = "USER_TABLE";
+    public static final String RIDE_ID = "ID";
+    public static final String RIDE_VEHICLE = "NAME";
+    public static final String RIDE_GAS = "DRIVER";
+    public static final String RIDE_DISTANCE = "AGE";
+    */
+
     public DbHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
