2898f57c0d9105f7847a4d615c6d0806ef4b7a52
danielbibit <danielbibit@gmail.com>
2016-12-16 15:11:08 -0200

    Edited the DbHelper class

--- app/src/main/java/daniel/broride/DbHelper.java
+++ app/src/main/java/daniel/broride/DbHelper.java
@@ -1,8 +1,24 @@
 package daniel.broride;
 
-/**
- * Created by danie on 16/12/2016.
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DbHelper {
+public class DbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "main.db";
+
+    public DbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/daniel/broride/SqlException.java
+++ app/src/main/java/daniel/broride/SqlException.java
@@ -1,8 +1,5 @@
 package daniel.broride;
 
-/**
- * Created by danie on 16/12/2016.
- */
-
 public class SqlException extends Exception {
+
 }
